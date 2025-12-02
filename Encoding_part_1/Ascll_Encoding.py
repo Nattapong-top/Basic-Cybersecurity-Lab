7dd676247807805b824b113d93ad591a5fbb3ccf
nattapong.top <nattapong.top@hotmail.com>
2025-12-02 17:24:49 +0700

    แปลงรหัส ascll เป็น  str

---
+++ Encoding_part_1/Ascll_Encoding.py
@@ -0,0 +1,8 @@
+a = '78 67 83 65 32 84 104 97 105 108 97 110 100'
+
+def ascll_code(a:str):
+    code = [chr(int(n)) for n in a.split()]
+    return code
+
+word_str = ascll_code(a)
+print(''.join(word_str))
